e22e083d9484ef200376a986c30c7255e14223a0
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-14 17:13:21 +0530

    Add files via upload

---
+++ toggle.java
@@ -0,0 +1,21 @@
+package set4;
+
+import java.util.Scanner;
+
+public class toggle {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String s=in.next();
+	char c[]=s.toCharArray();
+	String res="";
+	for(int i=0;i<s.length();i++){
+	if(c[i]>='a' && c[i]<='z'){
+		res=res+Character.toString(c[i]).toUpperCase();
+	}
+	else if(c[i]>='A' && c[i]<='Z'){
+		res=res+Character.toString(c[i]).toLowerCase();
+	}
+	}
+	System.out.println(res);
+}
+}
