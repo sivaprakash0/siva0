78c0a6e6f8f6b5fa89e705c0438d71ca6a5189f8
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-14 21:36:48 +0530

    Add files via upload

---
+++ stroccuran.java
@@ -0,0 +1,27 @@
+package set4;
+
+import java.util.Scanner;
+
+public class stroccuran {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String s=in.next();
+	String s1=in.next();
+	int l=s1.length();
+	int c=0,cnt=0;
+	for(int i=0;i<s.length()-2;i++){
+		String a=s.substring(i,i+l);
+		if(a.equals(s1)){
+			c=i;
+			cnt++;
+			break;
+		}
+	}
+	if(cnt==0){
+		System.out.println("-1");
+	}
+	else{
+		System.out.println(c);
+	}
+}
+}
